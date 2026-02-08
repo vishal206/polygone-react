e4af753af78c51735e152569ed707a08a957e27e
vishal206 <vishal206ani@gmail.com>
2026-02-08 16:21:37 +0530

    feat: basic sandbox with Sandpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import AnimationRenderer from "./sections/AnimationRenderer";
-import { DEMO_SPEC } from "./utils/constants";
+import {
+  SandpackCodeEditor,
+  SandpackFileExplorer,
+  SandpackLayout,
+  SandpackPreview,
+  SandpackProvider,
+} from "@codesandbox/sandpack-react";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <AnimationRenderer spec={DEMO_SPEC} />
+    <div className="overflow-hidden h-screen">
+      <SandpackProvider
+        template="react"
+        options={{
+          externalResources: ["https://cdn.tailwindcss.com"],
+        }}
+        customSetup={{
+          dependencies: {
+            react: "^18.2.0",
+            "react-dom": "^18.2.0",
+            gsap: "^3.12.5",
+            "@gsap/react": "^2.1.0",
+          },
+        }}
+        style={{ height: "100vh" }}
+      >
+        <SandpackLayout style={{ height: "100vh" }}>
+          <SandpackFileExplorer style={{ height: "100vh" }} />
+          <SandpackCodeEditor style={{ height: "100vh" }} />
+          <SandpackPreview style={{ height: "100vh" }} />
+        </SandpackLayout>
+      </SandpackProvider>
     </div>
   );
 }
